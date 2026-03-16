4b3b0a5cd83df039088f7584512b965faf04878f
Ahmet Buğra <bugrakacdi@gmail.com>
2026-03-16 15:22:02 +0300

    Initial Commit

---
+++ MathPro/MathProApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathProApp.swift
+//  MathPro
+//
+//  Created by Ahmet Buğra  on 16.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct MathProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
